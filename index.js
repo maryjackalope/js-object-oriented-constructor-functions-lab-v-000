3b8aa630e4cbdb937452cca675f7fba8b6ff983a
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 20:28:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function Scooter (year, color, model){
+  this.year = 2015
+  this.color = red 
+  this.model = swoosh
+}
+
+function Driver (name, age, experience){
+  this.name = Allison
+  this.age =
+}
+
+function PickupLocation (address, city){
+  
+}
