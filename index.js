e17c03c29b3b2f96b5ea77f333d4b424ea41285b
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 20:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function Scooter (year, color, model){
 let swoosh = new Scooter('swoosh', 2017, 'red')
 
 function Driver (name, age, experience){
-  this.name = Allison
+  this.name = name 
+  this.age = age
+  this.experience = expereience
  
 }
 let Allison = new Driver('Allison', '16 years' , '2 years')
