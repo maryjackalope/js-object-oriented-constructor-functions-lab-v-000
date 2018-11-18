50dd4d37ced10f6b63ca92c1d872a5a7a008d142
Mary Jack <maryjackinthebox@gmail.com>
2018-11-18 20:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function Scooter (year, color, model){
   this.model = model
 }
 
+let bobby = new Scooter('bobby', 20, 'Philadelphia')
+
 function Driver (name, age, experience){
   this.name = Allison
  
